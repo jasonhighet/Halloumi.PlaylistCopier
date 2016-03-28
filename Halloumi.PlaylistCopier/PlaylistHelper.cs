c382c994786c067a763d0481d290284b5ce30846
Jason Highet <jason.highet@gmail.com>
2016-03-29 12:28:34 +1300

    Removed complexity from playlist helper

--- Halloumi.PlaylistCopier/PlaylistHelper.cs
+++ Halloumi.PlaylistCopier/PlaylistHelper.cs
@@ -14,208 +14,45 @@ namespace Halloumi.PlaylistCopier
         /// </summary>
         /// <param name="playlistFile">The playlist file.</param>
         /// <returns>A list of file names contained in the playlist</returns>
-        public static List<PlaylistEntry> GetPlaylistEntries(string playlistFile)
+        public static List<string> GetPlaylistFiles(string playlistFile)
         {
-            var playlistEntries = new List<PlaylistEntry>();
+            var files = new List<string>();
             using (var reader = new StreamReader(playlistFile, Encoding.UTF7))
             {
                 string currentLine;
                 while ((currentLine = reader.ReadLine()) != null)
                 {
                     if (currentLine.Length <= 0 || currentLine == "#EXTM3U") continue;
-                    var playlistEntry = new PlaylistEntry();
 
                     if (currentLine.StartsWith("#"))
-                    {
-                        var elements = currentLine.Split(',').ToList();
-                        if (elements.Count > 0)
-                        {
-                            elements = currentLine.Substring(currentLine.IndexOf(',') + 1).Split('-').ToList();
-                            if (elements.Count == 2)
-                            {
-                                playlistEntry.Artist = elements[0].Trim();
-                                playlistEntry.Title = elements[1].Trim();
-                            }
-                            else
-                            {
-                                playlistEntry.Title = string.Join("-", elements.ToArray());
-                                playlistEntry.Artist = "";
-                            }
-                        }
                         currentLine = reader.ReadLine();
-                    }
-
                     if (currentLine == null) continue;
 
-                    string path;
+
+                    string file;
                     if (currentLine.StartsWith(@"\"))
                     {
-                        path = Path.GetPathRoot(playlistFile) + currentLine;
-                        playlistEntry.Path = path;
-                        playlistEntry.Path = playlistEntry.Path.Replace(@"\\", @"\");
+                        file = Path.GetPathRoot(playlistFile) + currentLine;
                     }
                     else if (currentLine.Contains(":"))
                     {
-                        path = currentLine;
+                        file = currentLine;
                     }
                     else
                     {
                         var directoryName = Path.GetDirectoryName(playlistFile);
-                        path = Path.Combine(directoryName + "", currentLine);
+                        file = Path.Combine(directoryName + "", currentLine);
                     }
 
-                    var trackDetails = GuessTrackDetailsFromFilename(path.Trim());
-                    if (playlistEntry.Title == "") playlistEntry.Title = trackDetails.Title;
-                    if (playlistEntry.Artist == "") playlistEntry.Artist = trackDetails.Artist;
-                    playlistEntry.Description = trackDetails.Description;
+                    file = file.Trim().Replace(@"\\", @"\");
 
-                    playlistEntry.Path = path.Trim();
-
-                    if (playlistEntry.Path != "")
-                    {
-                        playlistEntries.Add(playlistEntry);
-                    }
+                    if (file != "")
+                        files.Add(file);
                 }
                 reader.Close();
             }
-            return playlistEntries;
+            return files;
         }
 
-        /// <summary>
-        ///     Guesses the artist and title of a track from its filename.
-        /// </summary>
-        /// <param name="filename">The filename.</param>
-        /// <returns>A guess at the artist and filename</returns>
-        private static TrackDetails GuessTrackDetailsFromFilename(string filename)
-        {
-            filename = (Path.GetFileNameWithoutExtension(filename) + "").Replace("_", " ").Trim();
-            var elements = filename.Split('-').ToList();
-
-            var trackDetails = new TrackDetails
-            {
-                Artist = "",
-                Title = "",
-                Description = ""
-            };
-
-            if (elements.Count > 3) for (var i = 3; i < elements.Count; i++) elements[2] += "-" + elements[i];
-
-            switch (elements.Count)
-            {
-                case 1:
-                    trackDetails.Title = elements[0].Trim();
-                    break;
-                case 2:
-                    trackDetails.Artist = elements[0].Trim();
-                    trackDetails.Title = elements[1].Trim();
-                    break;
-                case 3:
-                    int trackNumber;
-                    if (int.TryParse(elements[0], out trackNumber))
-                    {
-                        trackDetails.Artist = elements[1].Trim();
-                        trackDetails.Title = elements[2].Trim();
-                    }
-                    else
-                    {
-                        trackDetails.Artist = elements[0].Trim();
-                        trackDetails.Title = (elements[1] + "-" + elements[2]).Trim();
-                    }
-                    break;
-            }
-
-            if (trackDetails.Artist.ToLower().StartsWith("various") || trackDetails.Title.Contains("  "))
-            {
-                trackDetails.Title = trackDetails.Title.Replace("  ", "/");
-                elements = trackDetails.Title.Split('/').ToList();
-                if (elements.Count == 2)
-                {
-                    trackDetails.Artist = elements[0].Trim();
-                    trackDetails.Title = elements[1].Trim();
-                }
-            }
-
-            trackDetails.Description = GuessTrackDescription(filename, trackDetails.Artist, trackDetails.Title);
-
-            return trackDetails;
-        }
-
-        /// <summary>
-        ///     Guesses the track description.
-        /// </summary>
-        /// <param name="filename">The filename.</param>
-        /// <param name="artist">The artist.</param>
-        /// <param name="title">The title.</param>
-        /// <returns>The track description</returns>
-        public static string GuessTrackDescription(string filename, string artist, string title)
-        {
-            string description;
-
-            if (artist != "" && title != "")
-            {
-                description = $"{artist} - {title}";
-            }
-            else
-            {
-                description = filename.Trim();
-
-                var regex = new Regex("various artists", RegexOptions.IgnoreCase);
-                description = regex.Replace(description, "");
-
-                regex = new Regex("various artist", RegexOptions.IgnoreCase);
-                description = regex.Replace(description, "");
-
-                regex = new Regex("various", RegexOptions.IgnoreCase);
-                description = regex.Replace(description, "");
-
-                regex = new Regex("[0-9]+", RegexOptions.IgnoreCase);
-                description = regex.Replace(description, "");
-
-                description = description.Replace("_", " ");
-                description = description.Replace(".", " ");
-
-                description = StringHelper.TitleCase(description.Trim());
-            }
-
-            return description;
-        }
-
-
-        /// <summary>
-        ///     Represents an entry in a m3u playlist
-        /// </summary>
-        public class PlaylistEntry
-        {
-            public PlaylistEntry()
-            {
-                Path = "";
-                Artist = "";
-                Title = "";
-            }
-
-            public string Path { get; set; }
-            public string Artist { get; set; }
-            public string Title { get; set; }
-            public string Description { get; set; }
-        }
-
-        /// <summary>
-        ///     Track details
-        /// </summary>
-        private class TrackDetails
-        {
-            public TrackDetails()
-            {
-                Artist = "";
-                Title = "";
-                Description = "";
-            }
-
-            public string Title { get; set; }
-
-            public string Artist { get; set; }
-
-            public string Description { get; set; }
-        }
     }
 }
--- Halloumi.PlaylistCopier/frmMain.cs
+++ Halloumi.PlaylistCopier/frmMain.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using Halloumi.Common.Windows.Forms;
 using System.IO;
+using System.Linq;
 using Halloumi.Common.Helpers;
+using Halloumi.Common.Windows.Forms;
 
 namespace Halloumi.PlaylistCopier
 {
+    // ReSharper disable once InconsistentNaming
     public partial class frmMain : BaseForm
     {
         /// <summary>
-        /// Gets or sets the playlist files.
-        /// </summary>
-        private List<PlaylistHelper.PlaylistEntry> PlaylistEntries { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the frmMain class.
+        ///     Initializes a new instance of the frmMain class.
         /// </summary>
         public frmMain()
         {
@@ -29,32 +21,33 @@ namespace Halloumi.PlaylistCopier
         }
 
         /// <summary>
-        /// Copies the files.
+        ///     Gets or sets the playlist files.
+        /// </summary>
+        private List<string> PlaylistFiles { get; set; }
+
+        /// <summary>
+        ///     Copies the files.
         /// </summary>
         private void CopyFiles()
         {
-            this.PlaylistEntries = PlaylistHelper.GetPlaylistEntries(txtPlaylist.Text);
-            progressDialog.Maximum = this.PlaylistEntries.Count;
+            PlaylistFiles = PlaylistHelper.GetPlaylistFiles(txtPlaylist.Text);
+            progressDialog.Maximum = PlaylistFiles.Count;
             progressDialog.Start();
         }
 
         /// <summary>
-        /// Determines whether this form is valid.
+        ///     Determines whether this form is valid.
         /// </summary>
-        /// <returns>True if this form is valid; otherwise, false.
+        /// <returns>
+        ///     True if this form is valid; otherwise, false.
         /// </returns>
         private bool IsValid()
         {
-            var valid = true;
-
-            if (!txtPlaylist.IsValid()) valid = false;
-            if (!txtDestinationFolder.IsValid()) valid = false;
-
-            return valid;
+            return txtPlaylist.IsValid();
         }
 
         /// <summary>
-        /// Handles the Click event of the btnCopy control.
+        ///     Handles the Click event of the btnCopy control.
         /// </summary>
         private void btnCopy_Click(object sender, EventArgs e)
         {
@@ -62,7 +55,7 @@ namespace Halloumi.PlaylistCopier
         }
 
         /// <summary>
-        /// Handles the CustomValidate event of the txtPlaylist control.
+        ///     Handles the CustomValidate event of the txtPlaylist control.
         /// </summary>
         private void txtPlaylist_CustomValidate(object sender, CancelEventArgs e)
         {
@@ -70,7 +63,7 @@ namespace Halloumi.PlaylistCopier
         }
 
         /// <summary>
-        /// Handles the CustomValidate event of the txtDestinationFolder control.
+        ///     Handles the CustomValidate event of the txtDestinationFolder control.
         /// </summary>
         private void txtDestinationFolder_CustomValidate(object sender, CancelEventArgs e)
         {
@@ -78,7 +71,7 @@ namespace Halloumi.PlaylistCopier
         }
 
         /// <summary>
-        /// Handles the TextChanged event of the txtPlaylist control.
+        ///     Handles the TextChanged event of the txtPlaylist control.
         /// </summary>
         private void txtPlaylist_TextChanged(object sender, EventArgs e)
         {
@@ -87,40 +80,44 @@ namespace Halloumi.PlaylistCopier
 
             try
             {
-                var playlistEntries = PlaylistHelper.GetPlaylistEntries(txtPlaylist.Text);
-                playlistCount = playlistEntries.Count();
-                existCount = playlistEntries.Count(plf => File.Exists(plf.Path));
+                var playlistFiles = PlaylistHelper.GetPlaylistFiles(txtPlaylist.Text);
+                playlistCount = playlistFiles.Count();
+                existCount = playlistFiles.Count(File.Exists);
             }
-            catch 
-            { }
+            catch
+            {
+                // ignored
+            }
 
             var nonexistCount = playlistCount - existCount;
 
-            lblPlaylistDetails.Text = string.Format("{0} files in playlist.", playlistCount);
-            if(nonexistCount > 0)
+            lblPlaylistDetails.Text = $"{playlistCount} files in playlist.";
+            if (nonexistCount > 0)
             {
-                lblPlaylistDetails.Text+= string.Format(" {0} could not be found.", nonexistCount);
+                lblPlaylistDetails.Text += $" {nonexistCount} could not be found.";
             }
         }
 
         /// <summary>
-        /// Handles the PerformProcessing event of the progressDialog control.
+        ///     Handles the PerformProcessing event of the progressDialog control.
         /// </summary>
         private void progressDialog_PerformProcessing(object sender, EventArgs e)
         {
             var destinationFolder = txtDestinationFolder.Text;
 
-            foreach (var playlistEntry in this.PlaylistEntries)
+            foreach (var playlistEntry in PlaylistFiles.TakeWhile(playlistEntry => !progressDialog.Cancelled))
             {
-                if (progressDialog.Cancelled) break;
+                progressDialog.Text = "Copying playlist track " + Path.GetFileName(playlistEntry);
+                progressDialog.Details += "Copying playlist track " + Path.GetFileName(playlistEntry) + "...";
 
-                progressDialog.Text = "Copying playlist track " + playlistEntry.Description;
-                progressDialog.Details += "Copying playlist track " + playlistEntry.Description + "...";
+                if (playlistEntry == null)
+                    continue;
 
-                var destination = Path.Combine(destinationFolder, Path.GetFileName(playlistEntry.Path));
+                var destination = Path.Combine(destinationFolder, Path.GetFileName(playlistEntry));
+
                 try
                 {
-                    FileSystemHelper.Copy(playlistEntry.Path, destination);
+                    FileSystemHelper.Copy(playlistEntry, destination);
                     if (progressDialog.Cancelled) break;
                     if (progressDialog.Cancelled) break;
 
@@ -128,17 +125,24 @@ namespace Halloumi.PlaylistCopier
                 }
                 catch (Exception exception)
                 {
-                    string message = String.Format("{0}ERROR: Could not copy file '{1}' to '{2}'{0}{3}{0}",
+                    var message = string.Format("{0}ERROR: Could not copy file '{1}' to '{2}'{0}{3}{0}",
                         Environment.NewLine,
-                        Path.GetFileName(playlistEntry.Path),
+                        Path.GetFileName(playlistEntry),
                         destinationFolder,
                         exception.Message);
+
                     progressDialog.Details += message;
 
                     if (File.Exists(destination))
                     {
-                        try { File.Delete(destination); }
-                        catch { }
+                        try
+                        {
+                            File.Delete(destination);
+                        }
+                        catch
+                        {
+                            // ignored
+                        }
                     }
                 }
                 progressDialog.Value++;
